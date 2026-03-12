daef9db1583fadd61590eb7bedbc05eedc26ba9e
Adicondro <adicondro@proton.me>
2026-03-12 19:17:36 +0700

    making a password generator

---
+++ minimalpw.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 20
+
+char allowed_characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+
+int main()
+{
+  srand(time(NULL));
+  char password[PASSWORD_LENGTH+1];
+  for(int i = 0; i < PASSWORD_LENGTH; i++){
+    int random_number = rand() % (sizeof(allowed_characters) - 1);
+    password[i] = allowed_characters[random_number];
+  }
+  password[PASSWORD_LENGTH] = 0;
+  printf("Your password is: %s\n", password);
+
+}
